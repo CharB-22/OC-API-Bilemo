48649aa0a5178d46e318d09634be302b3973cd9d
Charlène Bennevault <c.bennevault@gmail.com>
2021-07-23 16:43:27 +0200

    Create the client Controller and the GET Client list route

---
+++ src/Controller/ApiClientController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Client;
+use App\Repository\ClientRepository;
+use App\Entity\EndUser;
+use App\Repository\EndUserRepository;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+class ApiClientController extends AbstractController
+{
+    /**
+     * @Route("/api/clients", name="api_clients_list", methods={"GET"})
+     */
+    public function getClientList(ClientRepository $clientRepository): Response
+    {
+        
+        return $this->json($clientRepository->findAll(), 200, [], ['groups' => 'clients:read']);
+    }   
+}
