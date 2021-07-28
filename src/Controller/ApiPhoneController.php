eb4dd0e3d6d9b4030371a84398c897e31a8cd81b
Charlène Bennevault <c.bennevault@gmail.com>
2021-07-28 17:07:53 +0200

    Add documentation details on Phone Controller

--- src/Controller/ApiPhoneController.php
+++ src/Controller/ApiPhoneController.php
@@ -4,18 +4,34 @@ namespace App\Controller;
 
 use App\Repository\PhoneRepository;
 use App\Entity\Phone;
-use JMS\Serializer\Serializer;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 use JMS\Serializer\SerializerInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Annotations as OA;
 
 
 class ApiPhoneController extends AbstractController
 {
     /**
+     * List the phone selection available at Bilemo.
+     * 
      * @Route("/api/phones", name="api_phones_list", methods = {"GET"})
+     * 
+     * @OA\Response(
+     *     response=200,
+     *     description="Returns the list of phones.",
+     *     @OA\JsonContent(
+     *         type="array",
+     *         @OA\Items(ref=@Model(type=Phone::class))
+     *     )
+     * )
+     * @Security(name="Bearer")
      */
     public function getPhoneList(PhoneRepository $phoneRepository, SerializerInterface $serializer): Response
     {
@@ -29,7 +45,19 @@ class ApiPhoneController extends AbstractController
     }
 
     /**
+     * Give the details on a selected phone.
+     * 
      * @Route("/api/phones/{id}", name="api_phone_details", methods = {"GET"})
+     * 
+     * @OA\Response(
+     *     response=200,
+     *     description="Returns the details of a phone object.",
+     *     @OA\JsonContent(
+     *         type="array",
+     *         @OA\Items(ref=@Model(type=Phone::class))
+     *     )
+     * )
+     * @Security(name="Bearer")
      */
     public function getPhoneDetails(PhoneRepository $phoneRepository, 
     Phone $phone, 
